0b3324925bf36173d95ec158d9cfd1cbead06173
Lisa-moto <lisamarie181002@gmail.com>
2024-10-21 21:58:31 +0200

    3 beispiele mehr

--- Thema 1 - N-Body Problem/main.cpp
+++ Thema 1 - N-Body Problem/main.cpp
@@ -9,6 +9,9 @@ int main() {
     vector<vector<double>> table = importTable(file);
 
     cout << table[1][1] << endl;
+    cout << table[0][0] << endl;
+    cout << table[0][4] << endl;
+    cout << table[1][4] << endl;
 
 
     return 0;
