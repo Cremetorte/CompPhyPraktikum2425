cc0067702b6f26baf8015e5f6fbdc926f97ad6df
cremetorte <deschwab@t-online.de>
2024-10-31 12:25:10 +0100

    cleaned up

--- Thema1_N-Body-Problem/Test.cpp
+++ Thema1_N-Body-Problem/Test.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main() {
     
     //Test Velocity-Verlet with 2-body
-    string file = "Input/2body.csv";
+    string file = "Input/3body.csv";
     vector<vector<double>> table = importData(file);
 
     vector<vector<double>> processed_data = process_data(table);
@@ -22,18 +22,22 @@ int main() {
     print_data(processed_data);
     cout << endl;
 
-    double t_max = 2;
+    double t_max = 220;
     double eta = 0.001;
     double nr_steps = t_max/eta;
-
-    cout << "-------------------Calculation-------------------" << endl;
+    
+    cout << "Calculating " << nr_steps << " steps" << endl;
+    
     for (int step = 0; step < nr_steps; step++){
         //cout << "step " << step << endl;
-        vector<vector<double>> evolved_data = velocity_verlet(processed_data, eta, 2);
+        vector<vector<double>> evolved_data = velocity_verlet(processed_data, eta, 3);
         //print_data(acceleration(evolved_data));
 
         //concatenate processed data and evolved data
         processed_data.insert(processed_data.end(), evolved_data.begin(),evolved_data.end());
+        if (step % 10000 == 0) {
+            cout << "step " << step << "/" << nr_steps << endl;
+        }
     }
 
     //print_data(processed_data);
@@ -42,7 +46,7 @@ int main() {
     print_Vector(dimensions(processed_data));
 
 
-    string output_file = "Output/2Body_Velocity_Verlet.csv";
+    string output_file = "Output/3-body/3Body_Velocity_Verlet.csv";
     write_to_csv(processed_data, output_file);
  
 
